c635622fd34b7807b964e3e2412bde1ac7062dc9
321gillian <gillian.middleton@gmail.com>
2023-02-12 14:34:07 +0000

    Use StringUtils and dependency to lang3

--- src/main/java/ait/course/example/Application.java
+++ src/main/java/ait/course/example/Application.java
@@ -1,6 +1,12 @@
 package ait.course.example;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+  public int CountWords(String words) {
+    String[] separateWords = StringUtils.split(words, ' ');
+    return (separateWords == null) ? 0 : separateWords.length;
+  }
 
   public Application() {
     System.out.println("Inside Application");
@@ -10,5 +16,7 @@ public class Application {
   public static void main(final String[] args) {
     System.out.println("Starting Application");
     Application app = new Application();
+    int count = app.CountWords("I have four words");
+    System.out.println("Word count: " + count);
   }
 }
